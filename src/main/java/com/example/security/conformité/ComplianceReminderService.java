35bed1e61c764fecc4d5eac21f455466ca2dd1e0
AmeniLagha <laghaamani3@gmail.com>
2026-04-27 17:14:17 +0100

    email automatique

--- src/main/java/com/example/security/conformité/ComplianceReminderService.java
+++ src/main/java/com/example/security/conformité/ComplianceReminderService.java
@@ -64,7 +64,7 @@ public class ComplianceReminderService {
     /**
      * Tâche programmée qui s'exécute tous les jours à 9h00
      */
-    @Scheduled(cron = "0 05 17 * * *" , zone = "Africa/Tunis")
+    @Scheduled(cron = "0 18 17 * * *" , zone = "Africa/Tunis")
     public void sendPendingComplianceReminders() {
         log.info("🔔 Démarrage de la vérification des rappels de conformité");
         long count = receptionHistoryRepository.count();
